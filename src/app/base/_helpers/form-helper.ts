5b64e3dbc7a0a776cca07e4b5b2263cc60c3ef52
Nguyễn Văn Giáp <giap.nguyenvan.562@gmail.com>
2021-06-01 17:23:03 +0700

    upcode

--- src/app/base/_helpers/form-helper.ts
+++ src/app/base/_helpers/form-helper.ts
@@ -1,18 +1,54 @@
-import {FormGroup} from '@angular/forms';
+import {FormControl, FormGroup} from '@angular/forms';
 import {AppInjector} from '../../app-injector';
 import {ActivatedRoute, ActivatedRouteSnapshot, Router} from '@angular/router';
 import {BaseService} from '../_services/base.service';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Subject} from 'rxjs';
 
-export abstract class FormHelper<T> { // for 1 fields
+@Component({
+  template: ``
+})
+// tslint:disable-next-line:component-class-suffix
+export abstract class FormHelper<T> implements OnInit, OnDestroy { // for 1 fields
   abstract fields = [];
-  form: FormGroup = new FormGroup({});
+  form: FormGroup;
   submitting = false;
+  protected onDestroy$: Subject<boolean> = new Subject<boolean>();
+
+  private activatedRoute: ActivatedRoute;
+  private baseService: BaseService<T>;
+
+  protected constructor(activated: ActivatedRoute, base: BaseService<T>) {
+    this.activatedRoute = activated;
+    this.baseService = base;
+  }
+
+  ngOnInit(): void {
+    this.activatedRoute.data.subscribe(data => {
+      if (data.object) {
+        this.form = this.buildForm(data.object);
+        console.log('form', this.form.controls)
+      }
+    });
+  }
+
+  buildForm(data: Partial<T> = {}): FormGroup {
+    const form = new FormGroup({});
+    this.fields.forEach(field => {
+      const formControl = new FormControl(data[field.key]);
+      form.addControl(field.key, formControl);
+    });
+    return form;
+  }
 
 
-  handleSubmit() {
+  handleSubmit(): void {
 
   }
 
-  constructor(activatedRoute: ActivatedRoute, baseService: BaseService<T>) {
+
+  ngOnDestroy(): void {
+    this.onDestroy$.next(true);
+    this.onDestroy$.complete();
   }
 }
--- src/app/user/_resolvers/user.resolver.ts
+++ src/app/user/_resolvers/user.resolver.ts
@@ -1,13 +1,14 @@
-import {ActivatedRouteSnapshot, Resolve, RouterStateSnapshot} from '@angular/router';
+import {ActivatedRouteSnapshot, Resolve, Router, RouterStateSnapshot} from '@angular/router';
 import {UserEntity} from '../_models/user.entity';
 import {UserService} from '../_services/user.service';
 import {Observable, of} from 'rxjs';
 import {Injectable} from '@angular/core';
+import {catchError} from 'rxjs/operators';
 
 
 @Injectable({providedIn: 'root'})
 export class UserResolver implements Resolve<UserEntity> {
-  constructor(private userService: UserService) {
+  constructor(private userService: UserService, private router: Router) {
   }
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<UserEntity> {
@@ -15,7 +16,13 @@ export class UserResolver implements Resolve<UserEntity> {
     if (id === 'create') {
       return of(new UserEntity());
     } else {
-      return this.userService.get(id);
+      return this.userService.get(id).pipe(
+        catchError(error => {
+            this.router.navigateByUrl('/404');
+            return of(null);
+          }
+        )
+      );
     }
   }
 }
